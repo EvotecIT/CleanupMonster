c94a5bb40deb0b863795fe294e77bbb25f41295c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-03-05 11:09:53 +0100

    Interactive usage

---
+++ Examples/DeleteComputersInteractive.ps1
@@ -0,0 +1,7 @@
+﻿Import-Module .\CleanupActiveDirectory.psd1 -Force
+
+
+#$Output = Invoke-ADComputersCleanup -Disable -WhatIfDisable -ShowHTML
+$Output = Invoke-ADComputersCleanup -Delete -WhatIfDelete -ShowHTML
+#$Output = Invoke-ADComputersCleanup -Delete -WhatIf
+$Output
