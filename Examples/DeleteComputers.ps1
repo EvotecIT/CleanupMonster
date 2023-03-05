4a8ca7d4e6bc34d4279a6afb8fa1bae6ba313d71
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-03-05 11:09:31 +0100

    Full example

---
+++ Examples/DeleteComputers.ps1
@@ -0,0 +1,42 @@
+﻿Import-Module .\CleanupActiveDirectory.psd1 -Force
+
+# Run the script
+$Configuration = @{
+    DisableNoServicePrincipalName  = $null
+    DisableLastLogonDateMoreThan   = 90
+    DisablePasswordLastSetMoreThan = 90
+    DisableExcludeSystems          = @(
+        # 'Windows Server*'
+    )
+    DisableIncludeSystems          = @()
+    DeleteIsEnabled                = $false
+    DeleteNoServicePrincipalName   = $null
+    DeleteLastLogonDateMoreThan    = 180
+    DeletePasswordLastSetMoreThan  = 180
+    DeleteListProcessedMoreThan    = 90 # 90 days since computer was added to list
+    DeleteExcludeSystems           = @(
+        # 'Windows Server*'
+    )
+    DeleteIncludeSystems           = @(
+
+    )
+    DeleteLimit                    = 2 # 0 means unlimited, ignored for reports
+    DisableLimit                   = 2 # 0 means unlimited, ignored for reports
+    DisableModifyDescription       = $true
+    Exclusions                     = @(
+        'OU=Domain Controllers'
+        #'OU=Servers,OU=Production'
+    )
+    Filter                         = '*'
+    ReportOnly                     = $false
+    WhatIfDisable                  = $true
+    WhatIfDelete                   = $true
+    LogPath                        = "$PSScriptRoot\Logs\DeleteComputers_$((Get-Date).ToString('yyyy-MM-dd_HH_mm_ss')).log"
+    ListProcessed                  = "$PSScriptRoot\DeleteComputers_ListProcessed.xml"
+    ReportPath                     = "$PSScriptRoot\Reports\DeleteComputers_$((Get-Date).ToString('yyyy-MM-dd_HH_mm_ss')).html"
+    ShowHTML                       = $true
+}
+
+# Run one time as admin: Write-Event -ID 10 -LogName 'Application' -EntryType Information -Category 0 -Message 'Initialize' -Source 'CleanupComputers'
+$Output = Invoke-ADComputersCleanup @Configuration -EnableDisableFeature -EnableDeleteFeature
+$Output
