76b751f3c2ae4aaf20dda79a9b4b3843d7fc3bd1
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-03-05 11:09:44 +0100

    Example to enable writting to Application log

---
+++ Examples/DeleteComputersEnableSource.ps1
@@ -0,0 +1,3 @@
+﻿Import-Module $PSScriptRoot\Modules\PSEventViewer\PSEventViewer.psd1 -Force
+
+Write-Event -ID 10 -LogName 'Application' -EntryType Information -Category 0 -Message 'Initialize' -Source 'CleanupComputers'
