b84874d9094d53e0c931a777448854942c0c58f2
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-03-05 19:22:55 +0100

    Improve html report

--- Private/New-HTMLProcessedComputers.ps1
+++ Private/New-HTMLProcessedComputers.ps1
@@ -1,7 +1,7 @@
 ﻿function New-HTMLProcessedComputers {
     [CmdletBinding()]
     param(
-        [System.Collections.IDictionary] $Report,
+        [System.Collections.IDictionary] $Export,
         [string] $FilePath,
         [switch] $Online,
         [switch] $ShowHTML,
@@ -14,14 +14,46 @@
         New-HTMLPanelStyle -BorderRadius 0px
         New-HTMLTableOption -DataStore JavaScript -BoolAsString -ArrayJoinString ', ' -ArrayJoin
 
-        New-HTMLTab -Name 'Computers Pending Deletion' {
-            New-HTMLTable -DataTable $Report.ReportPendingDeletion
+        New-HTMLHeader {
+            New-HTMLSection -Invisible {
+                New-HTMLSection {
+                    New-HTMLText -Text "Report generated on $(Get-Date)" -Color Blue
+                } -JustifyContent flex-start -Invisible
+                New-HTMLSection {
+                    New-HTMLText -Text "Active Directory Cleanup - $($Export['Version'])" -Color Blue
+                } -JustifyContent flex-end -Invisible
+            }
         }
-        New-HTMLTab -Name 'Computers Disabled in Last Run' {
-            New-HTMLTable -DataTable $Report.ReportDisabled
+
+        New-HTMLTab -Name 'Devices Current Run' {
+            New-HTMLTable -DataTable $Export.CurrentRun -Filtering -ScrollX {
+                New-HTMLTableCondition -Name 'WhatIf' -ComparisonType string -Value 'True' -BackgroundColor LightYellow -FailBackgroundColor LightBlue
+                New-HTMLTableCondition -Name 'Action' -ComparisonType string -Value 'Delete' -BackgroundColor PinkLace
+                New-HTMLTableCondition -Name 'Action' -ComparisonType string -Value 'Disable' -BackgroundColor EnergyYellow
+                New-HTMLTableCondition -Name 'ActionStatus' -ComparisonType string -Value 'True' -BackgroundColor LightGreen
+                New-HTMLTableCondition -Name 'ActionStatus' -ComparisonType string -Value 'False' -BackgroundColor Salmon
+                New-HTMLTableCondition -Name 'ActionStatus' -ComparisonType string -Value 'Whatif' -BackgroundColor LightBlue
+            }
         }
-        New-HTMLTab -Name 'Computers Deleted in Last Run' {
-            New-HTMLTable -DataTable $Report.ReportDeleted
+        New-HTMLTab -Name 'Devices History' {
+            New-HTMLTable -DataTable $Export.History -Filtering -ScrollX {
+                New-HTMLTableCondition -Name 'WhatIf' -ComparisonType string -Value 'True' -BackgroundColor LightYellow -FailBackgroundColor LightBlue
+                New-HTMLTableCondition -Name 'Action' -ComparisonType string -Value 'Delete' -BackgroundColor PinkLace
+                New-HTMLTableCondition -Name 'Action' -ComparisonType string -Value 'Disable' -BackgroundColor EnergyYellow
+                New-HTMLTableCondition -Name 'ActionStatus' -ComparisonType string -Value 'True' -BackgroundColor LightGreen
+                New-HTMLTableCondition -Name 'ActionStatus' -ComparisonType string -Value 'False' -BackgroundColor Salmon
+                New-HTMLTableCondition -Name 'ActionStatus' -ComparisonType string -Value 'Whatif' -BackgroundColor LightBlue
+            }
+        }
+        New-HTMLTab -Name 'Devices Pending' {
+            New-HTMLTable -DataTable $Export.PendingDeletion -Filtering -ScrollX {
+                New-HTMLTableCondition -Name 'WhatIf' -ComparisonType string -Value 'True' -BackgroundColor LightYellow -FailBackgroundColor LightBlue
+                New-HTMLTableCondition -Name 'Action' -ComparisonType string -Value 'Delete' -BackgroundColor PinkLace
+                New-HTMLTableCondition -Name 'Action' -ComparisonType string -Value 'Disable' -BackgroundColor EnergyYellow
+                New-HTMLTableCondition -Name 'ActionStatus' -ComparisonType string -Value 'True' -BackgroundColor LightGreen
+                New-HTMLTableCondition -Name 'ActionStatus' -ComparisonType string -Value 'False' -BackgroundColor Salmon
+                New-HTMLTableCondition -Name 'ActionStatus' -ComparisonType string -Value 'Whatif' -BackgroundColor LightBlue
+            }
         }
         if ($LogFile -and (Test-Path -LiteralPath $LogFile)) {
             $LogContent = Get-Content -Raw -LiteralPath $LogFile
