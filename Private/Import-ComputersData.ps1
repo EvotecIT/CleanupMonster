806aecff15d08b84748c39adb51ba57439274ccf
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-03-20 19:22:48 +0100

    Move function to a different file

---
+++ Private/Import-ComputersData.ps1
@@ -0,0 +1,37 @@
+﻿function Import-ComputersData {
+    [CmdletBinding()]
+    param(
+        [string] $DataStorePath,
+        [System.Collections.IDictionary] $Export
+    )
+
+
+    try {
+        if ($DataStorePath -and (Test-Path -LiteralPath $DataStorePath)) {
+            $FileImport = Import-Clixml -LiteralPath $DataStorePath -ErrorAction Stop
+            #$ProcessedComputers = Import-Clixml -LiteralPath $FilePath -ErrorAction Stop
+            if ($FileImport.PendingDeletion) {
+                if ($FileImport.PendingDeletion.GetType().Name -ne 'Hashtable') {
+                    Write-Color -Text "[i] ", "Incorrecting XML format. PendingDeletion is not a hashtable. Terminating." -Color Yellow, Red
+                    return
+                }
+            }
+            if ($FileImport.History) {
+                if ($FileImport.History.GetType().Name -ne 'ArrayList') {
+                    Write-Color -Text "[i] ", "Incorrecting XML format. History is not a ArrayList. Terminating." -Color Yellow, Red
+                    return
+                }
+            }
+            $ProcessedComputers = $FileImport.PendingDeletion
+            $Export['History'] = $FileImport.History
+        }
+        if (-not $ProcessedComputers) {
+            $ProcessedComputers = [ordered] @{ }
+        }
+    } catch {
+        Write-Color -Text "[i] ", "Couldn't read the list or wrong format. Error: $($_.Exception.Message)" -Color Yellow, Red
+        return
+    }
+
+    $ProcessedComputers
+}
