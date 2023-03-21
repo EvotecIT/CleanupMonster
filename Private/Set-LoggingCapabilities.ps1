f9d8da048b617da2877bffe689fd175f137ad532
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-03-21 12:29:56 +0100

    Make sure to not respect WHATIF

--- Private/Set-LoggingCapabilities.ps1
+++ Private/Set-LoggingCapabilities.ps1
@@ -6,7 +6,7 @@
     if ($Configuration.LogPath) {
         $FolderPath = [io.path]::GetDirectoryName($Configuration.LogPath)
         if (-not (Test-Path -LiteralPath $FolderPath)) {
-            $null = New-Item -Path $FolderPath -ItemType Directory -Force
+            $null = New-Item -Path $FolderPath -ItemType Directory -Force -WhatIf:$false
         }
         $Script:PSDefaultParameterValues = @{
             "Write-Color:LogFile" = $Configuration.LogPath
@@ -17,7 +17,7 @@
             Write-Color -Text '[i] ', "Logs directory has more than ", $Configuration.LogMaximum, " log files. Cleanup required..." -Color Yellow, DarkCyan, Red, DarkCyan
             foreach ($Log in $CurrentLogs) {
                 try {
-                    Remove-Item -LiteralPath $Log.FullName -Confirm:$false
+                    Remove-Item -LiteralPath $Log.FullName -Confirm:$false -WhatIf:$false
                     Write-Color -Text '[+] ', "Deleted ", "$($Log.FullName)" -Color Yellow, White, Green
                 } catch {
                     Write-Color -Text '[-] ', "Couldn't delete log file $($Log.FullName). Error: ', "$($_.Exception.Message) -Color Yellow, White, Red
