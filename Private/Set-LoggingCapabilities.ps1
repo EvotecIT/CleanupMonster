615dc071af9ebeed3e977b2b3c29db415767273e
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-04-01 20:10:10 +0200

    improve log cleanup

--- Private/Set-LoggingCapabilities.ps1
+++ Private/Set-LoggingCapabilities.ps1
@@ -1,28 +1,32 @@
 ﻿function Set-LoggingCapabilities {
     [CmdletBinding()]
     param(
-        [System.Collections.IDictionary] $Configuration
+        [string] $LogPath,
+        [int] $LogMaximum
     )
-    if ($Configuration.LogPath) {
-        $FolderPath = [io.path]::GetDirectoryName($Configuration.LogPath)
+    if ($LogPath) {
+        $FolderPath = [io.path]::GetDirectoryName($LogPath)
         if (-not (Test-Path -LiteralPath $FolderPath)) {
             $null = New-Item -Path $FolderPath -ItemType Directory -Force -WhatIf:$false
         }
         $Script:PSDefaultParameterValues = @{
-            "Write-Color:LogFile" = $Configuration.LogPath
+            "Write-Color:LogFile" = $LogPath
         }
-
-        $CurrentLogs = Get-ChildItem -LiteralPath $FolderPath | Sort-Object -Property CreationTime -Descending | Select-Object -Skip $Configuration.LogMaximum
-        if ($CurrentLogs) {
-            Write-Color -Text '[i] ', "Logs directory has more than ", $Configuration.LogMaximum, " log files. Cleanup required..." -Color Yellow, DarkCyan, Red, DarkCyan
-            foreach ($Log in $CurrentLogs) {
-                try {
-                    Remove-Item -LiteralPath $Log.FullName -Confirm:$false -WhatIf:$false
-                    Write-Color -Text '[+] ', "Deleted ", "$($Log.FullName)" -Color Yellow, White, Green
-                } catch {
-                    Write-Color -Text '[-] ', "Couldn't delete log file $($Log.FullName). Error: ', "$($_.Exception.Message) -Color Yellow, White, Red
+        if ($LogMaximum -gt 0) {
+            $CurrentLogs = Get-ChildItem -LiteralPath $FolderPath | Sort-Object -Property CreationTime -Descending | Select-Object -Skip $LogMaximum
+            if ($CurrentLogs) {
+                Write-Color -Text '[i] ', "Logs directory has more than ", $LogMaximum, " log files. Cleanup required..." -Color Yellow, DarkCyan, Red, DarkCyan
+                foreach ($Log in $CurrentLogs) {
+                    try {
+                        Remove-Item -LiteralPath $Log.FullName -Confirm:$false -WhatIf:$false
+                        Write-Color -Text '[+] ', "Deleted ", "$($Log.FullName)" -Color Yellow, White, Green
+                    } catch {
+                        Write-Color -Text '[-] ', "Couldn't delete log file $($Log.FullName). Error: ', "$($_.Exception.Message) -Color Yellow, White, Red
+                    }
                 }
             }
+        } else {
+            Write-Color -Text '[i] ', "LogMaximum is set to 0 (Unlimited). No log files will be deleted." -Color Yellow, DarkCyan
         }
     }
 }
