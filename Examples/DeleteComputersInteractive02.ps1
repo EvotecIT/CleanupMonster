7779e6c15ec02057d644405aaa601278b5e178bf
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-04-01 20:17:28 +0200

    Update example

--- Examples/DeleteComputersInteractive02.ps1
+++ Examples/DeleteComputersInteractive02.ps1
@@ -1,6 +1,6 @@
 ﻿Import-Module .\CleanupActiveDirectory.psd1 -Force
 
-$Output = Invoke-ADComputersCleanup -LogMaximum 0 -DeleteIsEnabled $false -Delete -WhatIfDelete -ShowHTML -ReportOnly -LogPath $PSScriptRoot\Logs\DeleteComputers_$((Get-Date).ToString('yyyy-MM-dd_HH_mm_ss')).log -ReportPath $PSScriptRoot\Reports\DeleteComputers_$((Get-Date).ToString('yyyy-MM-dd_HH_mm_ss')).html
+$Output = Invoke-ADComputersCleanup -LogMaximum 4 -ReportMaximum 4 -DeleteIsEnabled $false -Delete -WhatIfDelete -ShowHTML -ReportOnly -LogPath $PSScriptRoot\Logs\DeleteComputers_$((Get-Date).ToString('yyyy-MM-dd_HH_mm_ss')).log -ReportPath $PSScriptRoot\Reports\DeleteComputers_$((Get-Date).ToString('yyyy-MM-dd_HH_mm_ss')).html
 $Output
 
 $Output = Invoke-ADComputersCleanup -DeleteListProcessedMoreThan 100 -Disable -DeleteIsEnabled $false -Delete -WhatIfDelete -ShowHTML -ReportOnly -LogPath $PSScriptRoot\Logs\DeleteComputers_$((Get-Date).ToString('yyyy-MM-dd_HH_mm_ss')).log -ReportPath $PSScriptRoot\Reports\DeleteComputers_$((Get-Date).ToString('yyyy-MM-dd_HH_mm_ss')).html
