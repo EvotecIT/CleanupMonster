208ac4f013055be3869147e9757fa28bf4ce96c8
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-04-17 23:40:38 +0200

    add example

--- Examples/DeleteComputersInteractive.ps1
+++ Examples/DeleteComputersInteractive.ps1
@@ -4,6 +4,14 @@
 $Output = Invoke-ADComputersCleanup -WhatIf -ReportOnly -Disable -Delete -ShowHTML
 $Output
 
+# this is a fresh run and it will provide report only according to its defaults
+# the defaults are 180 days last logon and 180 days password last set
+# but we also now add the requirement that computer hasn't changed it's password before 2021-08-19 00:00:00
+# and it hasn't logged on before 2021-08-19 00:00:00
+$DateTime = Get-Date -Year 2021 -Month 8 -Day 19 -Hour 0 -Minute 0 -Second 0
+$Output = Invoke-ADComputersCleanup -WhatIf -ReportOnly -Disable -ShowHTML -DisablePasswordLastSetOlderThan $DateTime -DisableLastLogonDateOlderThan $DateTime -DeletePasswordLastSetOlderThan $DateTime -DeleteLastLogonDateOlderThan $DateTime
+$Output
+
 # this is a fresh run and it will try to disable computers according to it's defaults
 # read documentation to understand what it does
 $Output = Invoke-ADComputersCleanup -Disable -ShowHTML -WhatIfDisable -WhatIfDelete -Delete
