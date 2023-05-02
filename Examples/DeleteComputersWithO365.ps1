91abf054133507b61e234c3faa76ae3220610896
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-05-02 21:37:15 +0200

    Example for AD cleanup with O365 data

---
+++ Examples/DeleteComputersWithO365.ps1
@@ -0,0 +1,7 @@
+﻿Import-Module .\CleanupMonster.psd1 -Force
+
+Connect-MgGraph -Scopes Device.Read.All, DeviceManagementManagedDevices.Read.All, Directory.ReadWrite.All, DeviceManagementConfiguration.Read.All
+
+# this is a fresh run and it will provide report only according to it's defaults
+$Output = Invoke-ADComputersCleanup -WhatIf -ReportOnly -Disable -ShowHTML -DisableLastSeenAzureMoreThan 80 -DisableLastSyncAzureMoreThan 80 -DisableLastSeenIntuneMoreThan 80
+$Output
