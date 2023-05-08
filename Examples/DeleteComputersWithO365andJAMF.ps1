9136ff1e7713e2f334845e5e366b4eadb4c483c8
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-05-08 09:53:09 +0200

    add example for jamf/o365

---
+++ Examples/DeleteComputersWithO365andJAMF.ps1
@@ -0,0 +1,52 @@
+﻿Import-Module .\CleanupMonster.psd1 -Force
+
+# connect to graph for Azure AD, Intune (requires GraphEssentials module)
+Connect-MgGraph -Scopes Device.Read.All, DeviceManagementManagedDevices.Read.All, Directory.ReadWrite.All, DeviceManagementConfiguration.Read.All
+# connect to jamf (requires PowerJamf module)
+Connect-Jamf -Organization 'aaa' -UserName 'aaa' -Suppress -Force -PasswordEncrypted 'aaaaa'
+
+$invokeADComputersCleanupSplat = @{
+    # safety limits (minimum amount of computers that has to be returned from each source)
+    SafetyADLimit                  = 40
+    SafetyAzureADLimit             = 5
+    SafetyIntuneLimit              = 3
+    SafetyJamfLimit                = 50
+    # disable settings
+    Disable                        = $true
+    DisableLimit                   = 10
+    DisableLastLogonDateMoreThan   = 90
+    DisablePasswordLastSetMoreThan = 90
+    DisableLastSeenAzureMoreThan   = 90
+    DisableLastSyncAzureMoreThan   = 90
+    DisableLastContactJamfMoreThan = 90
+    DisableLastSeenIntuneMoreThan  = 90
+    # delete settings
+    Delete                         = $true
+    DeleteLimit                    = 10
+    DeleteLastLogonDateMoreThan    = 180
+    DeletePasswordLastSetMoreThan  = 180
+    DeleteLastSeenAzureMoreThan    = 180
+    DeleteLastSyncAzureMoreThan    = 180
+    DeleteLastContactJamfMoreThan  = 180
+    DeleteLastSeenIntuneMoreThan   = 180
+    DeleteListProcessedMoreThan    = 90 # disabled computer has to spend 90 days in list before it can be deleted
+    DeleteIsEnabled                = $false # Computer has to be disabled to be deleted
+    # global exclusions
+    Exclusions                     = @(
+        '*OU=Domain Controllers*' # exclude Domain Controllers
+    )
+    # filter for AD search
+    Filter                         = '*'
+    # logs, reports and datastores
+    LogPath                        = "$PSScriptRoot\Logs\CleanupComputers_$((Get-Date).ToString('yyyy-MM-dd_HH_mm_ss')).log"
+    DataStorePath                  = "$PSScriptRoot\CleanupComputers_ListProcessed.xml"
+    ReportPath                     = "$PSScriptRoot\Reports\CleanupComputers_$((Get-Date).ToString('yyyy-MM-dd_HH_mm_ss')).html"
+    # WhatIf settings
+    #ReportOnly                     = $true
+    WhatIfDisable                  = $true
+    WhatIfDelete                   = $true
+    ShowHTML                       = $true
+}
+
+$Output = Invoke-ADComputersCleanup @invokeADComputersCleanupSplat
+$Output
