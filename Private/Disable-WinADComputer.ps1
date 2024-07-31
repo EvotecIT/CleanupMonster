c1bc67d4db3bf39b1c3d7d4326dccddc035a8d7c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-07-31 09:51:53 +0200

    Move logic to separate functions for order change ability

---
+++ Private/Disable-WinADComputer.ps1
@@ -0,0 +1,45 @@
+﻿function Disable-WinADComputer {
+    [CmdletBinding(SupportsShouldProcess)]
+    param(
+        [bool] $Success,
+        [switch] $WhatIfDisable,
+        [switch] $DontWriteToEventLog,
+        [PSCustomObject] $Computer,
+        [string] $Server
+    )
+    if ($Success) {
+        if ($Computer.Enabled -eq $true) {
+            Write-Color -Text "[i] Disabling computer ", $Computer.SamAccountName, ' DN: ', $Computer.DistinguishedName, ' Enabled: ', $Computer.Enabled, ' Operating System: ', $Computer.OperatingSystem, ' LastLogon: ', $Computer.LastLogonDate, " / " , $Computer.LastLogonDays , ' days, PasswordLastSet: ', $Computer.PasswordLastSet, " / ", $Computer.PasswordLastChangedDays, " days" -Color Yellow, Green, Yellow, Green, Yellow, Green, Yellow, Green, Yellow, Green, Yellow, Green, Yellow, Green
+            try {
+                if ($Computer.DistinguishedNameAfterMove) {
+                    $DN = $Computer.DistinguishedNameAfterMove
+                } else {
+                    $DN = $Computer.DistinguishedName
+                }
+
+                Disable-ADAccount -Identity $DN -Server $Server -WhatIf:$WhatIfDisable -ErrorAction Stop
+                Write-Color -Text "[+] Disabling computer ", $DN, " (WhatIf: $WhatIfDisable) successful." -Color Yellow, Green, Yellow
+                if (-not $DontWriteToEventLog) {
+                    Write-Event -ID 10 -LogName 'Application' -EntryType Information -Category 1000 -Source 'CleanupComputers' -Message "Disabling computer $($Computer.SamAccountName) successful." -AdditionalFields @('Disable', $Computer.SamAccountName, $Computer.DistinguishedName, $Computer.Enabled, $Computer.OperatingSystem, $Computer.LastLogonDate, $Computer.PasswordLastSet, $WhatIfDisable) -WarningAction SilentlyContinue -WarningVariable warnings
+                }
+                foreach ($W in $Warnings) {
+                    Write-Color -Text "[-] ", "Warning: ", $W -Color Yellow, Cyan, Red
+                }
+                $Success = $true
+            } catch {
+                $Computer.ActionComment = $_.Exception.Message
+                $Success = $false
+                Write-Color -Text "[-] Disabling computer ", $DN, " (WhatIf: $WhatIfDisable) failed. Error: $($_.Exception.Message)" -Color Yellow, Red, Yellow
+                if (-not $DontWriteToEventLog) {
+                    Write-Event -ID 10 -LogName 'Application' -EntryType Error -Category 1001 -Source 'CleanupComputers' -Message "Disabling computer $($Computer.SamAccountName) failed. Error: $($_.Exception.Message)" -AdditionalFields @('Disable', $Computer.SamAccountName, $Computer.DistinguishedName, $Computer.Enabled, $Computer.OperatingSystem, $Computer.LastLogonDate, $Computer.PasswordLastSet, $WhatIfDisable, $($_.Exception.Message)) -WarningAction SilentlyContinue -WarningVariable warnings
+                }
+                foreach ($W in $Warnings) {
+                    Write-Color -Text "[-] ", "Warning: ", $W -Color Yellow, Cyan, Red
+                }
+            }
+        } else {
+            Write-Color -Text "[i] Computer ", $Computer.SamAccountName, " is already disabled." -Color Yellow, Green, Yellow
+        }
+    }
+    $Success
+}
---
+++ Private/Move-WinADComputer.ps1
@@ -0,0 +1,44 @@
+﻿function Move-WinADComputer {
+    [CmdletBinding(SupportsShouldProcess)]
+    param(
+        [bool] $Success,
+        [bool] $DisableAndMove,
+        [System.Collections.IDictionary] $OrganizationalUnit,
+        [PSCustomObject] $Computer,
+        [switch] $WhatIfDisable,
+        [switch] $DontWriteToEventLog,
+        [string] $Server
+    )
+    if ($Success -and $DisableAndMove) {
+        # we only move if we successfully disabled the computer
+        if ($OrganizationalUnit[$Domain]) {
+            if ($Computer.OrganizationalUnit -eq $OrganizationalUnit[$Domain]) {
+                Write-Color -Text "[i] Computer ", $Computer.DistinguishedName, " is already in the correct OU." -Color Yellow, Green, Yellow
+            } else {
+                try {
+                    $MovedObject = Move-ADObject -Identity $Computer.DistinguishedName -WhatIf:$WhatIfDisable -Server $Server -ErrorAction Stop -Confirm:$false -TargetPath $OrganizationalUnit[$Domain] -PassThru
+                    Write-Color -Text "[+] Moving computer ", $Computer.DistinguishedName, " (WhatIf: $($WhatIfDisable.IsPresent)) successful." -Color Yellow, Green, Yellow
+                    if (-not $DontWriteToEventLog) {
+                        Write-Event -ID 11 -LogName 'Application' -EntryType Warning -Category 1000 -Source 'CleanupComputers' -Message "Moving computer $($Computer.SamAccountName) successful." -AdditionalFields @('Move', $Computer.SamAccountName, $Computer.DistinguishedName, $Computer.Enabled, $Computer.OperatingSystem, $Computer.LastLogonDate, $Computer.PasswordLastSet, $WhatIfDisable) -WarningAction SilentlyContinue -WarningVariable warnings
+                    }
+                    foreach ($W in $Warnings) {
+                        Write-Color -Text "[-] ", "Warning: ", $W -Color Yellow, Cyan, Red
+                    }
+                    $Computer.DistinguishedNameAfterMove = $MovedObject.DistinguishedName
+                    $Success = $true
+                } catch {
+                    $Success = $false
+                    Write-Color -Text "[-] Moving computer ", $Computer.DistinguishedName, " (WhatIf: $($WhatIfDisable.IsPresent)) failed. Error: $($_.Exception.Message)" -Color Yellow, Red, Yellow
+                    if (-not $DontWriteToEventLog) {
+                        Write-Event -ID 11 -LogName 'Application' -EntryType Error -Category 1000 -Source 'CleanupComputers' -Message "Moving computer $($Computer.SamAccountName) failed." -AdditionalFields @('Move', $Computer.SamAccountName, $Computer.DistinguishedName, $Computer.Enabled, $Computer.OperatingSystem, $Computer.LastLogonDate, $Computer.PasswordLastSet, $WhatIfDisable, $($_.Exception.Message)) -WarningAction SilentlyContinue -WarningVariable warnings
+                    }
+                    foreach ($W in $Warnings) {
+                        Write-Color -Text "[-] ", "Warning: ", $W -Color Yellow, Cyan, Red
+                    }
+                    $Computer.ActionComment = $Computer.ActionComment + [System.Environment]::NewLine + $_.Exception.Message
+                }
+            }
+        }
+    }
+    $Success
+}
