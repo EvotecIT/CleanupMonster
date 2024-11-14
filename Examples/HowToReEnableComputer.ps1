b88c76674d5525d89f62a6b61a5440480bbc1cc1
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-11-14 17:58:09 +0100

    Example to reenable computer

---
+++ Examples/HowToReEnableComputer.ps1
@@ -0,0 +1,8 @@
+﻿$Computers = @(
+    'de-muc-01'
+)
+$Test = $Computers | Sort-Object -Unique
+
+foreach ($Computer in $Test) {
+    Set-ADComputer -Identity $Computer -Server 'domain.loc' -Enabled $true
+}
