aae45eef24d7a0b03f181788e046f369fb6bac36
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-11-14 17:58:25 +0100

    Remove protection

---
+++ Examples/HowToRemoveProtectedFromDeletion.ps1
@@ -0,0 +1,8 @@
+﻿$Computers = @(
+    'ComputerToRemoveProtected$'
+)
+$Server = 'domain.com'
+foreach ($Computer in $Computers) {
+    $ADComputer = Get-ADComputer -Identity $Computer -Server $Server
+    Set-ADObject -Server $Server -Identity $ADComputer.DistinguishedName -ProtectedFromAccidentalDeletion $false -Verbose
+}
