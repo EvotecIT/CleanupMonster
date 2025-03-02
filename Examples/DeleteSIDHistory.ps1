1922ce88ecc217d9dfa1a6499728ae1d224188ed
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-03-02 20:33:57 +0100

    Add DeleteSIDHistory script for SID history cleanup
    - Introduced a new script to invoke SID history cleanup
    - Configured parameters for verbose output and domain inclusion
    - Set a removal limit for SID history entries

---
+++ Examples/DeleteSIDHistory.ps1
@@ -0,0 +1,21 @@
+﻿Import-Module .\CleanupMonster.psd1 -Force
+
+# Run the script
+$invokeADSIDHistoryCleanupSplat = @{
+    Verbose                 = $true
+    WhatIf                  = $true
+    IncludeSIDHistoryDomain = @(
+        'S-1-5-21-3661168273-3802070955-2987026695'
+    )
+    IncludeInternalOnly     = $true
+    RemoveLimit             = 2
+}
+
+$Output = Invoke-ADSIDHistoryCleanup @invokeADSIDHistoryCleanupSplat
+$Output
+
+<#
+S-1-5-21-3661168273-3802070955-2987026695
+S-1-5-21-853615985-2870445339-3163598659
+S-1-5-21-1928204107-2710010574-1926425344
+#>
