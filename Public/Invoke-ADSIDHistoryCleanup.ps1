f98c5d5ebdae4615c75c3d9213e03207a66ac1a5
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-03-07 21:34:02 +0100

    Refactor Invoke-ADSIDHistoryCleanup.ps1 to enhance filtering options
    - Replace IncludeInternalOnly and IncludeExternalOnly parameters with IncludeType and ExcludeType arrays.
    - Update logic to skip domains based on the new filtering criteria.
    - Comment out legacy code for clarity and future reference.

--- Public/Invoke-ADSIDHistoryCleanup.ps1
+++ Public/Invoke-ADSIDHistoryCleanup.ps1
@@ -10,8 +10,8 @@
         [string[]] $ExcludeSIDHistoryDomain,
         [nullable[int]] $RemoveLimit,
         [nullable[int]] $RemoveLimitObject,
-        [switch] $IncludeInternalOnly,
-        [switch] $IncludeExternalOnly
+        [ValidateSet('Internal', 'External', 'Unknown')][string[]] $IncludeType = @('Internal', 'External', 'Unknown'),
+        [ValidateSet('Internal', 'External', 'Unknown')][string[]] $ExcludeType = @()
     )
 
     $ProcessedSIDs = 0
@@ -45,40 +45,83 @@
     :TopLoop foreach ($Domain in $DomainNames) {
         [Array] $Objects = $Output[$Domain]
         # we need to get SID domain for the "internal domains" which were translated to their names
-        if ($Domain -notlike "S-1*") {
-            $TranslatedSID = foreach ($D in $Output.DomainSIDs.Keys) {
-                $SID = $Output['DomainSIDs'][$D]
-                if ($SID.Domain -eq $Domain) {
-                    $Output.DomainSIDs[$D]
-                    break
-                }
-            }
-            if ($TranslatedSID) {
-                $DomainSID = $TranslatedSID.SID
-            } else {
-                $DomainSID = $Domain
-            }
-        } else {
-            $DomainSID = $Domain
-        }
+        # if ($Domain -notlike "S-1*") {
+        #     $TranslatedSID = foreach ($D in $Output.DomainSIDs.Keys) {
+        #         $SID = $Output['DomainSIDs'][$D]
+        #         if ($SID.Domain -eq $Domain) {
+        #             $Output.DomainSIDs[$D]
+        #             break
+        #         }
+        #     }
+        #     if ($TranslatedSID) {
+        #         $DomainSID = $TranslatedSID.SID
+        #     } else {
+        #         $DomainSID = $Domain
+        #     }
+        # } else {
+        #     $DomainSID = $Domain
+        # }
 
         if ($null -ne $RemoveLimitObject -and $GlobalLimitObject -ge $RemoveLimitObject) {
             break TopLoop
         }
 
+        if ($ExcludeType.Count -gt 0) {
+            if ($ExcludeType -contains 'Internal') {
+                if ($Output.DomainSIDs[$Domain] -and $Output.DomainSIDs[$Domain].Type -eq 'Domain') {
+                    Write-Color -Text "[s] ", "Skipping ", $Domain, " for domain ", $Output.DomainSIDs[$Domain].Domain, " as it's internal." -Color Yellow, White, Red, White, Red
+                    continue
+                }
+            }
+            if ($ExcludeType -contains 'External') {
+                if ($Output.DomainSIDs[$Domain] -and $Output.DomainSIDs[$Domain].Type -eq 'Trust') {
+                    Write-Color -Text "[s] ", "Skipping ", $Domain, " for domain ", $Output.DomainSIDs[$Domain].Domain, " as it's external (trusted)." -Color Yellow, White, Red, White, Red
+                    continue
+                }
+            }
+            if ($ExcludeType -contains 'Unknown') {
+                if (-not $Output.DomainSIDs[$Domain]) {
+                    Write-Color -Text "[s] ", "Skipping ", $Domain, " as it's unknown type." -Color Yellow, White, Red, White, Red
+                    continue
+                }
+            }
+        }
+
+        if ($IncludeType.Count -gt 0) {
+            # if ($IncludeType -contains 'Internal') {
+            #     if ($Output.DomainSIDs[$Domain] -and $Output.DomainSIDs[$Domain].Type -ne 'Domain') {
+            #         Write-Color -Text "[s] ", "Skipping ", $Domain, " for domain ", $Output.DomainSIDs[$Domain].Domain, " as it's not internal." -Color Yellow, White, Red, White, Red
+            #         continue
+            #     }
+            # }
+            # if ($IncludeType -contains 'External') {
+            #     if ($Output.DomainSIDs[$Domain] -and $Output.DomainSIDs[$Domain].Type -ne 'Trust') {
+            #         Write-Color -Text "[s] ", "Skipping ", $Domain, " for domain ", $Output.DomainSIDs[$Domain].Domain, " as it's not external." -Color Yellow, White, Red, White, Red
+            #         continue
+            #     }
+            # }
+            # if ($IncludeType -contains 'Unknown') {
+            #     if ($Output.DomainSIDs[$Domain] -and $Output.DomainSIDs[$Domain].Type -ne 'Unknown') {
+            #         Write-Color -Text "[s] ", "Skipping ", $Domain, " for domain ", $Output.DomainSIDs[$Domain].Domain, " as it's not unknown." -Color Yellow, White, Red, White, Red
+            #         continue
+            #     }
+            # }
+        }
+
+
         # We need to filter global domains, on a global level
-        if ($IncludeInternalOnly) {
-            if ($Domain -like "S-1*") {
-                Write-Color -Text "[s] ", "Skipping ", $Domain, " with ", $DomainSID, " as it's not internal." -Color Yellow, White, Red, White, Red
-                continue
-            }
-        }
-        if ($IncludeExternalOnly) {
-            if ($Domain -notlike "S-1*") {
-                Write-Color -Text "[s] ", "Skipping ", $Domain, " with ", $DomainSID, " as it's not external." -Color Yellow, White, Red, White, Red
-                continue
-            }
-        }
+        # if ($IncludeInternalOnly) {
+        #     if ($Domain -like "S-1*") {
+        #         Write-Color -Text "[s] ", "Skipping ", $Domain, " with ", $DomainSID, " as it's not internal." -Color Yellow, White, Red, White, Red
+        #         continue
+        #     }
+        # }
+        # if ($IncludeExternalOnly) {
+        #     if ($Domain -notlike "S-1*") {
+        #         Write-Color -Text "[s] ", "Skipping ", $Domain, " with ", $DomainSID, " as it's not external." -Color Yellow, White, Red, White, Red
+        #         continue
+        #     }
+        # }
         if ($IncludeSIDHistoryDomain -and $IncludeSIDHistoryDomain -notcontains $DomainSID) {
             if ($Domain -notlike "S-1*") {
                 Write-Color -Text "[s] ", "Skipping ", $Domain, " with ", $DomainSID, " not included." -Color Yellow, White, Red, White, Red
@@ -107,9 +150,9 @@
             Write-Color -Text "[i] ", "Processing ", $Object.Name, " (" , "Domain: ", $Object.Domain, ", ObjectClass: ", $Object.ObjectClass, ", SIDHistoryCount: ", $Object.SIDHistory.Count, ", DN: ", $Object.DistinguishedName, ")" -Color Yellow, White, Green, White, Yellow, Green, Yellow, Green, Yellow, Green, Yellow, Green, White
 
             if ($IncludeSIDHistoryDomain -or $ExcludeSIDHistoryDomain -or $IncludeInternalOnly -or $IncludeExternalOnly) {
-                Remove-SIDHistoryPerDomain -Object $Object -QueryServer $QueryServer
+                # Remove-SIDHistoryPerDomain -Object $Object -QueryServer $QueryServer
             } else {
-                Remove-SIDHistoryPerUser -Object $Object -QueryServer $QueryServer
+                #Remove-SIDHistoryPerUser -Object $Object -QueryServer $QueryServer
             }
         }
     }
