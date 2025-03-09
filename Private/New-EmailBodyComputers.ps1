a64db9ff5181b38db645ca73dab9e2081600688a
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-03-09 21:03:54 +0100

    Add New-EmailBodyComputers function to generate summary emails
    - Create a new function to format and send summary emails for computer object cleanup
    - Include details on actions taken (delete/disable) and counts of affected objects
    - Format email content with appropriate styling and structure

---
+++ Private/New-EmailBodyComputers.ps1
@@ -0,0 +1,41 @@
+﻿function New-EmailBodyComputers {
+    [CmdletBinding()]
+    param(
+        [Array] $CurrentRun
+    )
+
+    [Array] $DisabledObjects = $CurrentRun | Where-Object { $_.Action -eq 'Disable' }
+    [Array] $DeletedObjects = $CurrentRun | Where-Object { $_.Action -eq 'Delete' }
+
+    $EmailBody = EmailBody -EmailBody {
+        EmailText -Text "Hello,"
+
+        EmailText -LineBreak
+
+        EmailText -Text "This is an automated email from Automations run on ", $Env:COMPUTERNAME, " on ", (Get-Date -Format 'yyyy-MM-dd HH:mm:ss'), " by ", $Env:UserName -Color None, Green, None, Green, None, Green -FontWeight normal, bold, normal, bold, normal, bold
+
+        EmailText -LineBreak
+
+        EmailText -Text "Following is a summary for the computer object cleanup:" -FontWeight bold
+        EmailList {
+            EmailListItem -Text "Objects actioned: ", $Output.CurrentRun.Count -Color None, Green -FontWeight normal, bold
+            EmailListItem -Text "Objects deleted: ", $DeletedObjects.Count -Color None, Salmon -FontWeight normal, bold
+            EmailListItem -Text "Objects disabled: ", $DisabledObjects.Count -Color None, Orange -FontWeight normal, bold
+        }
+
+        EmailText -Text "Following objects were actioned:" -LineBreak -FontWeight bold -Color Salmon
+        EmailTable -DataTable $Output.CurrentRun -HideFooter {
+            New-HTMLTableCondition -Name 'Action' -ComparisonType string -Value 'Delete' -BackgroundColor PinkLace -Inline
+            New-HTMLTableCondition -Name 'Action' -ComparisonType string -Value 'Disable' -BackgroundColor EnergyYellow -Inline
+            New-HTMLTableCondition -Name 'ActionStatus' -ComparisonType string -Value 'True' -BackgroundColor LightGreen -Inline
+            New-HTMLTableCondition -Name 'ActionStatus' -ComparisonType string -Value 'False' -BackgroundColor Salmon -Inline
+            New-HTMLTableCondition -Name 'ActionStatus' -ComparisonType string -Value 'Whatif' -BackgroundColor LightBlue -Inline
+        }
+
+        EmailText -LineBreak
+
+        EmailText -Text "Regards,"
+        EmailText -Text "Automations Team" -FontWeight bold
+    }
+    $EmailBody
+}
