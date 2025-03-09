c8c2e4c7b3815326a2a9c0f0adc90382a77daf19
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-03-09 21:42:59 +0100

    Add Remove-SIDHistory function to manage SID history entries
    - Implement functionality to remove individual or all SID history entries
    - Include support for processing limits and export history of actions
    - Utilize ShouldProcess for safe operations

---
+++ Private/Remove-SIDHistory.ps1
@@ -0,0 +1,46 @@
+﻿function Remove-SIDHistory {
+    [CmdletBinding(SupportsShouldProcess)]
+    param(
+        [Array] $ObjectsToProcess,
+        [System.Collections.IDictionary] $Export
+    )
+    foreach ($Item in $ObjectsToProcess) {
+        $Object = $Item.Object
+        $QueryServer = $Item.QueryServer
+
+        if ($LimitPerSID) {
+            # Process individual SIDs for this object
+            foreach ($SID in $Object.SIDHistory) {
+                if ($GlobalLimitSID -ge $RemoveLimit) {
+                    Write-Color -Text "[i] ", "Reached SID limit of ", $RemoveLimit, ". Stopping processing." -Color Yellow, White, Green, White
+                    break
+                }
+                if ($PSCmdlet.ShouldProcess("$($Object.Name) ($($Object.Domain))", "Remove SID History entry $SID")) {
+                    Write-Color -Text "[i] ", "Removing SID History entry $SID from ", $Object.Name -Color Yellow, White, Green
+                    #Set-ADObject -Identity $Object.DistinguishedName -Remove @{ SIDHistory = $SID } -Server $QueryServer
+                    $GlobalLimitSID++
+                    $ProcessedSIDs++
+                }
+            }
+        } else {
+            # Process all SIDs for this object at once
+            if ($PSCmdlet.ShouldProcess("$($Object.Name) ($($Object.Domain))", "Remove all SID History entries")) {
+                Write-Color -Text "[i] ", "Removing all SID History entries from ", $Object.Name -Color Yellow, White, Green
+                #Set-ADObject -Identity $Object.DistinguishedName -Clear SIDHistory -Server $QueryServer
+                $ProcessedSIDs += $Object.SIDHistory.Count
+                $ProcessedObjects++
+            }
+        }
+
+        $Export.History.Add(
+            [PSCustomObject]@{
+                ObjectDN     = $Object.DistinguishedName
+                ObjectName   = $Object.Name
+                ObjectDomain = $Object.Domain
+                SID          = $SID
+                Action       = 'Remove'
+                ActionDate   = Get-Date
+            }
+        )
+    }
+}
