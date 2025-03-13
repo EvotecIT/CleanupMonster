ab2a9c8e73a019d8e369f215ed9d8854a978a60f
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-03-13 15:48:17 +0100

    Refactor Invoke-ADSIDHistoryCleanup to streamline SID removal process
    - Replaced manual SID processing loop with a call to Remove-SIDHistory function
    - Simplified code structure for better readability and maintainability

--- Public/Invoke-ADSIDHistoryCleanup.ps1
+++ Public/Invoke-ADSIDHistoryCleanup.ps1
@@ -248,36 +248,7 @@
 
     if (-not $ReportOnly) {
         # Process the collected objects for SID removal
-        foreach ($Item in $ObjectsToProcess) {
-            $Object = $Item.Object
-            $QueryServer = $Item.QueryServer
-
-            if ($LimitPerSID) {
-                # Process individual SIDs for this object
-                foreach ($SID in $Object.SIDHistory) {
-                    if ($GlobalLimitSID -ge $RemoveLimit) {
-                        Write-Color -Text "[i] ", "Reached SID limit of ", $RemoveLimit, ". Stopping processing." -Color Yellow, White, Green, White
-                        break
-                    }
-
-                    if ($PSCmdlet.ShouldProcess("$($Object.Name) ($($Object.Domain))", "Remove SID History entry $SID")) {
-                        Write-Color -Text "[i] ", "Removing SID History entry $SID from ", $Object.Name -Color Yellow, White, Green
-                        #Set-ADObject -Identity $Object.DistinguishedName -Remove @{ SIDHistory = $SID } -Server $QueryServer
-                        $GlobalLimitSID++
-                        $ProcessedSIDs++
-                    }
-                }
-            } else {
-                # Process all SIDs for this object at once
-                if ($PSCmdlet.ShouldProcess("$($Object.Name) ($($Object.Domain))", "Remove all SID History entries")) {
-                    Write-Color -Text "[i] ", "Removing all SID History entries from ", $Object.Name -Color Yellow, White, Green
-                    #Set-ADObject -Identity $Object.DistinguishedName -Clear SIDHistory -Server $QueryServer
-                    $ProcessedSIDs += $Object.SIDHistory.Count
-                    $ProcessedObjects++
-                }
-            }
-        }
-
+        Remove-SIDHistory -ObjectsToProcess $ObjectsToProcess -Export $Export
     }
 
     $Export['ProcessedObjects'] = $ProcessedObjects
