d0624a714811b915c4d653055e11d52ecc2bb633
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-25 13:49:58 +0200

    fix: 🐛 correct `RemoveLimitObject` and update `ReportOnly` parameters
    * Set `RemoveLimitObject` to 1 for consistency with other limits.
    * Updated `ReportOnly` to false to ensure proper reporting behavior.
    * Added `IncludeOrganizationalUnit` for clarity in parameter usage.

--- Examples/DeleteSIDHistory.ps1
+++ Examples/DeleteSIDHistory.ps1
@@ -8,14 +8,17 @@ $invokeADSIDHistoryCleanupSplat = @{
         'S-1-5-21-3661168273-3802070955-2987026695'
         'S-1-5-21-853615985-2870445339-3163598659'
     )
+    IncludeOrganizationalUnit = @(
+        "OU=ITR01,DC=ad,DC=evotec,DC=xyz"
+    )
     #IncludeType             = 'External'
     RemoveLimitSID          = 1
-    RemoveLimitObject       = 2
+    RemoveLimitObject       = 1
     SafetyADLimit           = 1
     ShowHTML                = $true
     Online                  = $true
     DisabledOnly            = $false
-    #ReportOnly              = $true
+    ReportOnly              = $false
     LogPath                 = "C:\Temp\ProcessedSIDHistory.log"
     ReportPath              = "$PSScriptRoot\ProcessedSIDHistory.html"
     DataStorePath           = "$PSScriptRoot\ProcessedSIDHistory.xml"
@@ -26,7 +29,7 @@ $Output = Invoke-ADSIDHistoryCleanup @invokeADSIDHistoryCleanupSplat
 $Output | Format-Table -AutoSize
 
 # Lets send an email
-$EmailBody = $Output.EmailBody
+#$EmailBody = $Output.EmailBody
 
 #Connect-MgGraph -Scopes 'Mail.Send' -NoWelcome
 #Send-EmailMessage -To 'przemyslaw.klys@test.pl' -From 'przemyslaw.klys@test.pl' -MgGraphRequest -Subject "Automated SID Cleanup Report" -Body $EmailBody -Priority Low -Verbose
